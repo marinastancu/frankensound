b0a1c6db9a2f69d66d670491a122d0da79d1e78a
marinastancu <448358@student.fontys.nl>
2023-06-10 01:00:53 +0200

    restructure project

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,19 @@
+val kotlin_version: String = "1.8.21"
+val ktor_version: String = "2.3.1"
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("gradle-plugin", version = kotlin_version))
+    implementation(kotlin("serialization", version = kotlin_version))
+    implementation("io.ktor.plugin", "plugin", ktor_version)
+    implementation(gradleApi())
+    implementation(localGroovy())
+}
--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -5,9 +5,9 @@ val aws_version: String by project
 val jansi_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.8.20"
-    id("io.ktor.plugin") version "2.2.4"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
+    kotlin("jvm")
+    id("io.ktor.plugin")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 group = "com.frankensound"
