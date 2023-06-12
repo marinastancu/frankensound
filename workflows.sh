2875e5bd040772cf0e09fd9142e003cc07904838
marinastancu <448358@student.fontys.nl>
2023-06-12 14:44:17 +0200

    Added template and script for microservice CI pipelines; restructuring repository

---
+++ workflows.sh
@@ -0,0 +1,13 @@
+# read the microservices template
+TEMPLATE=$(cat .github/template.yml)
+
+# iterate over each microservice directory
+for MICROSERVICE in $(ls microservices); do
+    echo "generating workflow for microservice/${MICROSERVICE}"
+
+    # replace template route placeholder with route name
+    WORKFLOW=$(echo "${TEMPLATE}" | sed "s/{{MICROSERVICE}}/${MICROSERVICE}/g")
+
+    # save workflow to .github/workflows/{MICROSERVICE}
+    echo "${WORKFLOW}" > .github/workflows/${MICROSERVICE}.yaml
+done
