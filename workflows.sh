d624b1549eb692ba15193e24146beef8650e7c56
marinastancu <448358@student.fontys.nl>
2023-06-12 15:03:35 +0200

    Fixing directory references

--- workflows.sh
+++ workflows.sh
@@ -3,12 +3,11 @@ TEMPLATE=$(cat .github/template.yml)
 
 # iterate over each microservice directory
 for MICROSERVICE in $(ls microservices); do
-    echo "generating workflow for microservice/${MICROSERVICE}"
+    echo "generating workflow for microservices/${MICROSERVICE}"
 
     # replace template route placeholder with route name
     WORKFLOW=$(echo "${TEMPLATE}" | sed "s/{{MICROSERVICE}}/${MICROSERVICE}/g")
 
     # save workflow to .github/workflows/{MICROSERVICE}
-    cd ./.github/workflows/
-    echo "${WORKFLOW}" > ${MICROSERVICE}.yml
+    echo "${WORKFLOW}" > .github/workflows/${MICROSERVICE}.yml
 done
